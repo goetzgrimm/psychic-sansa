8c442c060ba743fad0f0176d1a64706dfad56f5f
Goetz Grimm <goetz.grimm@gmail.com>
2013-01-06 12:22:41 -0800

    Delete hello 1-3

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -10,9 +10,6 @@ public class Hello {
 		System.out.println("Hello, world!");
 		System.out.println("Hello, psychic-sansa2!");
 		System.out.println("Hello, world 1!");
-		System.out.println("Hello dev 1");
-		System.out.println("Hello dev 2");
-		System.out.println("Hello dev 3");
 		System.out.println("Hello dev 4");
 		System.out.println("Hello dev 6");
 		System.out.println("Hello dev 5");
