4d2d4867984e3396f96c803d8722bd7e416b8933
Goetz Grimm <goetz.grimm@chromeriver.com>
2013-01-19 13:34:39 -0800

    first, second, final change in cr-003

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -11,6 +11,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+		System.out.println("Hello, psychic-blah!");
 	}
 	
 	public void foo() {
