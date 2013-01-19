956e706542b0f659461e3f0d75d8a28aecf665c2
Goetz Grimm <goetz.grimm@chromeriver.com>
2013-01-19 12:29:22 -0800

    foo 1
    foo 2
    
    first change in cr-001
    
    Merge branch 'cr-001'
    
    Conflicts:
    	HelloWorld/src/Hello.java
    
    clipse

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -12,10 +12,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Hello, psychic-sansa2!");
-		System.out.println("Hello, world 1!");
-		System.out.println("Hello dev 4");
-		System.out.println("Hello dev 6");
-		System.out.println("Hello dev 5");
+		System.out.println("Hello, psychic-sansa2! master change");
 	}
 	
 	public void foo() {
