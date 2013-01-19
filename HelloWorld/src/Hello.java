73768316085702b355b5aa34652a5fbc05dbc1cc
Goetz Grimm <goetz.grimm@chromeriver.com>
2013-01-19 13:31:07 -0800

    revert to first version

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -11,12 +11,6 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello, psychic-sansa2!");
-		System.out.println("Hello, psychic-sansa2! master change");
-		System.out.println("Hello, world 1!");
-		System.out.println("Hello dev 6");
-		System.out.println("Hello dev 8");
-		System.out.println("Hello dev 7");
 	}
 	
 	public void foo() {
