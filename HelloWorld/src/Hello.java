5448c3aafbb5a6e0c18d37e1e133de53730b2512
Goetz Grimm <goetz.grimm@chromeriver.com>
2013-01-19 16:36:47 -0800

    master 003

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("Hello, psychic-sansa2! master 002");
+		System.out.println("Hello, psychic-sansa2! master 003");
 	}
 	
 	public void foo() {
