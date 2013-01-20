e6927bba56f59919b937925ac2a6fcdd7f12a091
Goetz Grimm <goetz.grimm@chromeriver.com>
2013-01-19 16:57:46 -0800

    cr001 4, 5

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -12,10 +12,18 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("Hello, psychic-blah cr-001 3");
 =======
 		System.out.println("Hello, psychic-sansa2! master 004");
 >>>>>>> 0293af3... master 004
+=======
+		System.out.println("Hello, psychic-blah cr-001 4");
+>>>>>>> 50837dc... cr001 4
+=======
+		System.out.println("Hello, psychic-blah cr-001 5");
+>>>>>>> 6dee07b... cr-001 5
 	}
 	
 	public void foo() {
